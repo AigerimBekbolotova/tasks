7153d132793cce300c548f37190ab6f40b00df46
aigerim <aigerimbekbolotova7@gmail.com>
2022-02-16 11:18:01 +0600

    init:added 16.02

---
+++ 16.02/script.js
@@ -0,0 +1,12 @@
+/********1*********/
+let camelCase = '1';
+let snake_case = '2';
+let PascalCase = '3';
+alert(`${camelCase} ${snake_case} ${PascalCase}`)
+console.log(`${camelCase} ${snake_case} ${PascalCase}`)
+
+/********2*********/
+let str1 = 'САЛАМ,';
+let str2 = 'БИШКЕК!';
+let str3 = str1 + ' ' + str2;
+console.log(str3);
