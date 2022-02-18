b9f8a52cdbfbcd74e5115986fb44784048fcdb8d
aigerim <aigerimbekbolotova7@gmail.com>
2022-02-18 11:04:55 +0600

    feat: start project 18.02

---
+++ 18.02/1/script.js
@@ -0,0 +1,60 @@
+/*********1 zadanie**********/
+
+let num6 = 10;
+num6 ++
+console.log(num6);
+
+let num7 = 10;
+num7 --
+console.log(num7);
+
+
+/********* 2 zadanie**********/
+
+ let name = 'Айгерим';
+ console.log('Привет, ' + name);
+ alert('Привет, ' + name);
+
+
+/********* 3 zadanie**********/
+
+let age = 28;
+console.log('Мне ' + age + '!');
+alert('Мне ' + age + '!');
+
+/********* 4 zadanie**********/
+
+let a = 17;
+let b = 10;
+let c = a - b;
+let d = 7;
+let result = c+d;
+console.log(result);
+alert(result);
+
+/********** 5 zadanie**********/
+
+let num1 = 47;
+num1 +=7;
+console.log(num1)
+alert(num1)
+
+let num2 = 47;
+num2 -=18;
+console.log(num2)
+alert(num2)
+
+let num3 = 47;
+num3 *= 10;
+console.log(num3)
+alert(num3)
+
+let num4 = 47;
+num4 /= 15;
+console.log(num4)
+alert(num4)
+
+
+
+
+
