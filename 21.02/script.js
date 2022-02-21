a153fe7ff2198f4d997cb7dde7b37c11fededbcb
aigerim <aigerimbekbolotova7@gmail.com>
2022-02-21 11:37:53 +0600

    feat: added 21.02

---
+++ 21.02/script.js
@@ -0,0 +1,58 @@
+/********************* Классная работа *******************/
+
+/******** && ******/
+
+/*let num = 56;
+let num2 =65;
+let result = num === 45 && num2 === 65;
+console.log(result);// false
+let result2 = num ===77 && num2 === 65;
+console.log(result2);//false
+let result3 = num === 56 && num2 === 79;
+console.log(result3);//false
+let result4 = num === 56 && num2 > 45;
+console.log(result4);*/
+
+/********** || ********/
+
+/*let age = 28;
+let result5 = age === 28 || age <27
+console.log(result5);//true
+let result6 = age === 28 || age === 28;
+console.log(result6)
+let result7 = age === 28 || age > 28;
+console.log(result7);// true
+let result8 = age === 27 || age === 27;
+console.log(result8);*/
+
+/********************* Домашняя работа *******************/
+let num1 = 1;
+num1 ++12;
+console.log(num1);
+alert(num1);
+
+let num2 = 1;
+num2 --14;
+console.log(num2);
+alert(num2);
+
+let num3 = 1;
+num3 *= 5;
+console.log(num3);
+alert(num3);
+
+let num4 = 1;
+num4 /= 7;
+console.log(num4);
+alert(num4);
+
+let num5 = 1;
+num5 1++;
+console.log(num5);
+alert(num5);
+
+let num6 = 1;
+num6 1--;
+console.log(num6);
+alert(num6);
+
