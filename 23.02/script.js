c75b8dac0c3a131b8da7c093bb42ab40fcc3a093
aigerim <aigerimbekbolotova7@gmail.com>
2022-02-23 11:57:49 +0600

    feat: added 23.02

---
+++ 23.02/script.js
@@ -0,0 +1,50 @@
+/***********  1 zadanie   ***********/
+/*1) Создайте объект заработных плат.
+    Назовите userSalaries.
+    Свойствами выступают: Ella, Sophia, Ellie
+Выведите на экран зарплату Ella, Sophia.*/
+
+let userSalaries = {
+     'Ella': 50000,
+     'Sophia': 60000,
+     'Ellie': 55000
+}
+console.log(userSalaries['Ella'])
+console.log(userSalaries['Sophia'])
+
+/***********  2 zadanie   ***********/
+/*2) Создайте объект с ключами a, b и c значениями 5 , 6 и 10.
+Найдите сумму его элементов.*/
+
+let num = {
+    a: 5,
+    b: 6,
+    c: 10
+}
+let result = (num.a +num.b + num.c);
+console.log(result);
+
+/***********  3 zadanie   ***********/
+
+/*let number;
+if (data === true){
+    number = 3;
+} else{
+    number = 5;
+}*/
+let number = true;
+let access = number === true ? 3 : 5;
+alert(access);
+
+
+/***********  4 zadanie   ***********/
+
+let name = prompt('Кто такой В.В.Путин?');
+if ( name === 'Президент'){
+    alert ('Верно');
+}
+else {
+    alert ('Не знаете?');
+}
+console.log(name)
+
