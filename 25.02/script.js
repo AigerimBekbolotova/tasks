8cd71ee3a4be2750b039152412a1ab70e6852146
aigerim <aigerimbekbolotova7@gmail.com>
2022-02-25 14:57:58 +0600

    feat: fixed script

--- 25.02/script.js
+++ 25.02/script.js
@@ -45,9 +45,16 @@ console.log(result);//107
     Квадрат числа 5 будет 25.*/
 
 let rate = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
+let rate2 = [];
 let square = 0;
-for (let i of rate) {
-    square = square + i * i;
-    console.log(`${square}`);
+for (let i = 0; i < rate.length; i++) {
+    square = rate[i] * rate[i];
+    console.log(square);
+    rate2.push(square);
 }
-console.log(`${square}`);
+let result2 = 0;
+for ( r = 0; r < rate2.length; r++){
+    result2 += rate2[r];
+}
+console.log(result2);
+
